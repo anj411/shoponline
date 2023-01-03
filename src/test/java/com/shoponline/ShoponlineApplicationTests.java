e3b9987e5536b1b1ba2b4ce62bbb838f6b854c81
anj411 <jenanjali5@gmail.com>
2023-01-03 17:10:45 +0530

    first commit

--- src/test/java/com/shoponline/ShoponlineApplicationTests.java
+++ src/test/java/com/shoponline/ShoponlineApplicationTests.java
@@ -51,6 +51,7 @@ class ShoponlineApplicationTests {
 		}
 
 		driver.close();
+		
 	}
 	
 	
@@ -84,8 +85,9 @@ class ShoponlineApplicationTests {
 		} else {
 			System.out.println("Failed to login");
 		}
-
+		
 		driver.close();
+		
 	}
 
 	
@@ -122,6 +124,7 @@ class ShoponlineApplicationTests {
 		driver.findElement(By.xpath("//*[@id='detail']/form/div[1]")).click();
 		
 		String size = null;
+		
 		if (driver.findElement(By.xpath("//*[@id='small']")).isSelected()) {
 			size = driver.findElement(By.xpath("//*[@id='small']")).getAttribute("value");
 			
@@ -156,9 +159,117 @@ class ShoponlineApplicationTests {
 		} else {
 			System.out.println("Failed to add");
 		}
-
+		
+		placeOrder(driver);
+		
 		driver.close();
+		
+	}
+	
+	// When Order is placed
+	
+	public void placeOrder(WebDriver driver) throws Exception {	
+		
+		driver.findElement(By.xpath("/html/body/nav/div/div[3]/ul/li[5]/a")).click();
+		Thread.sleep(2000); 
+		driver.get("http://localhost:8080/cart");
+		 
+		if (driver.findElement(By.xpath("//*[@id='log']")).isDisplayed()) {		
+			
+			Thread.sleep(2000);
+			
+			driver.get("http://localhost:8080/signin");
+			driver.findElement(By.xpath("//*[@id='userName']")).sendKeys("jenanjali5@gmail.com");
+			driver.findElement(By.xpath("//*[@id='userPassword']")).sendKeys("anjali");
+			driver.findElement(By.xpath("//*[@id='login']")).click();
+			
+			Thread.sleep(2000);
+			
+			driver.get("http://localhost:8080/home");
+			driver.findElement(By.xpath("/html/body/nav/div/div[3]/ul/li[5]/a")).click();
+			
+			Thread.sleep(2000); 
+			driver.get("http://localhost:8080/cart");		
+		} 
+		
+		driver.findElement(By.xpath("//*[@id=\"place\"]")).click();
+		Thread.sleep(2000);
+		
+		saveAddress(driver);				
+				
+	}
+	
+	// When Address is saved
+	
+	public void saveAddress(WebDriver driver) throws InterruptedException {
+		
+		driver.get("http://localhost:8080/placeorder");
+			
+		String name = driver.findElement(By.xpath("//*[@id='name']")).getAttribute("value");
+		if (name == null) {
+			driver.findElement(By.xpath("//*[@id='name']")).sendKeys("Pooja Meena");
+		}
+			
+		String mobile = driver.findElement(By.xpath("//*[@id='number']")).getAttribute("value");
+		if (mobile == null) {
+			driver.findElement(By.xpath("//*[@id='number']")).sendKeys("9185462362");
+		}
+			
+		String pinCode = driver.findElement(By.xpath("//*[@id='code']")).getAttribute("value");
+		if (pinCode == null) {
+			driver.findElement(By.xpath("//*[@id='code']")).sendKeys("303502");
+		}
+			
+		String city = driver.findElement(By.xpath("//*[@id='city']")).getAttribute("value");
+		if (city == null) {
+			driver.findElement(By.xpath("//*[@id='city']")).sendKeys("Dausa");
+		}
+		
+		String address = driver.findElement(By.xpath("//*[@id='address']")).getAttribute("value");
+		if (address == null) {
+			driver.findElement(By.xpath("//*[@id='address']")).sendKeys("Gopalpura Bypass road, Jaipur");
+		}
+			
+		driver.findElement(By.xpath("//*[@id='order']")).click();
+			
+		Thread.sleep(2000);
+			
+		String actualUrl = "http://localhost:8080/payment";
+		String expectedUrl = driver.getCurrentUrl();
 
+		Assert.assertEquals(actualUrl, expectedUrl);
+
+		if (actualUrl.equals(expectedUrl)) {
+			System.out.println("Address successfully saved");
+
+		} else {
+			System.out.println("Failed to save");
+		}
+		
+		confirmOrder(driver);
+			
+	}
+	
+	
+	// When Order is confirmed
+	
+	public void confirmOrder(WebDriver driver) throws InterruptedException {
+		
+		driver.findElement(By.xpath("//*[@id=\"confirm\"]")).click();
+		Thread.sleep(2000);
+		
+		String actualUrl = "http://localhost:8080/confirmorder";
+		String expectedUrl = driver.getCurrentUrl();
+		
+		Assert.assertEquals(actualUrl, expectedUrl);
+
+		if (actualUrl.equals(expectedUrl)) {
+			System.out.println("Order Sucessfully Confirmed");
+
+		} else {
+			System.out.println("Failed to order");
+		}
+		
 	}
 
 }
